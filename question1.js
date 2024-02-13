ba3c145043f149dacd3bfdafbf77d447296f5746
Agnes RUKUNDO <agnesrukundo1998@gmail.com>
2024-02-13 12:15:50 +0200

    arrays

---
+++ question1.js
@@ -0,0 +1,6 @@
+let colors = ['red','green','blue'];
+let add = colors.push('yellow');
+let removeLast = colors.pop();
+let second = colors[1];
+let isIncluded = colors.includes('orange');
+//this was all about method used modify string
