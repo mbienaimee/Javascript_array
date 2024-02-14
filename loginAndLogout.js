03676e432ca6f3ebf89f9e7f6ff153752b04f241
BIENAIMEE Marie Reine <mbienaimee@alustudent.com>
2024-02-14 16:14:20 +0200

    login

---
+++ loginAndLogout.js
@@ -0,0 +1,12 @@
+var user = {
+    email:'bienaimee@gmail.com',
+    name:'reine',
+    logout function (){
+        console.log(this.email,'has logged out')
+    },
+    login :function (){
+        console.log(this.email,'has logged in')
+    },
+
+}
+console.log(user)
